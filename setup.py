ce80c666ab6dd16ac089958cb1b6d68d9c614837
tqi_acarolina <tqi_acarolina@uolinc.com>
2022-11-06 22:32:24 -0300

    primeiros arquivos

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="package_name",
+    version="0.0.1",
+    author="carolsrd",
+    author_email="anacarolinadosreissilva@gmail.com",
+    description="Desafio DIO Unimed",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="my_github_repository_project_link"
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8'
+)
